22a1814072163a806a9a5b915f5040a5a8aca82c
Francis Dolbec <fdolbec123@gmail.com>
2018-11-19 03:39:47 -0500

    Ajout d'un user_agent (évite le 403 Forbidden). Ouverture de l'url avec le user_agent.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import urllib.request as url
 
-with url.urlopen("https://www.google.ca/") as response:
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.1'
+user_agent_end = '02 Safari/537.36'
+user_agent = user_agent + user_agent_end
+
+headers = {"User-Agent": user_agent}
+request = url.Request("https://www.google.ca/search?q=test", None, headers)
+with url.urlopen(request) as response:
     html = response.read()
     print(html)
